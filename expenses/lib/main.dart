18874f58888c708435bcedd2b2f29f7efe6e005f
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-05-03 16:17:33 -0300

    Cards de itens criados

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,11 +1,11 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:expenses/models/Transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
 
-class ExpensesApp extends StatelessWidget {  
-
+class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,28 +15,59 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis',
+      value: 598.98,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 300.00,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Despesas pessoais"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text('Grafico'),
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Despesas pessoais"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Grafico'),
             ),
-            Card(
-              child: Text('Lista de transações'),
-            )
-          ],
           ),
-      );
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text(
+                        tr.value.toString(),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(tr.title),
+                        Text(tr.date.toString()),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),            
+          ),
+        ],
+      ),
+    );
   }
 }
