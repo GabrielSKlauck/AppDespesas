3e803b32c0b0248f3a718ad561ad64d782942953
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-05-03 17:59:50 -0300

    mudança no tipo de variavel de titulo e valor

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -16,6 +16,9 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
     Transaction(
       id: 't1',
@@ -99,11 +102,13 @@ class MyHomePage extends StatelessWidget {
               padding: const EdgeInsets.all(10.0),
               child: Column(children: [
                 TextField(
+                  controller: titleController,
                   decoration: InputDecoration(
                     labelText: 'titulo',
                   ),
                 ),
                 TextField(
+                  controller: valueController,
                   decoration: InputDecoration(
                     labelText: 'Valor (R\$)',
                   ),
@@ -116,7 +121,10 @@ class MyHomePage extends StatelessWidget {
                         "Nova transação",
                         style: TextStyle(color: Colors.purple),
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        print(titleController);
+                        print(valueController);
+                      },
                     ),
                   ],
                 )
